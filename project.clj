6954d881966b9d68b4288280fcffa5b2ec91d441
SUNEEJ <suneejnair@gmail.com>
2015-04-10 00:55:20 +0200

    Added more documentation.

--- project.clj
+++ project.clj
@@ -5,5 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
-  :plugins [[codox "0.8.11"]]
-  :codox {:defaults {:doc/format :markdown}})
+  :plugins  [[codox "0.8.11"]])
--- src/poole_algebra/core.clj
+++ src/poole_algebra/core.clj
@@ -1,36 +1,45 @@
 (ns poole-algebra.core
  (:require [clojure.math.numeric-tower :as math])) 
 
-(defn vec-add [& v]
+(defn vec-add 
     "Adds any number of vectors and returns a resultant vector."
+    [& v]
     (vec (reduce #(map + % %2) v)))
 
-(defn scalar-mult [c v]
+(defn scalar-mult 
     "Scalar multiplies vector v with c and returns a vector."
+    [c v]
     (vec (map #(* c %) v)))
 
-(defn neg [v]
+(defn neg 
     "Returns the negative of the vector."
+    {:doc/format :markdown}
+    [v]
     (map - v))
 
-(defn dot-prod [& v]
+(defn dot-prod 
     "Returns the dot product of the vectors."
+    [& v]
     (reduce + (reduce #(map * % %2) v)))
 
-(defn vec-len [v]
+(defn vec-len 
     "Returns the length of the vector v."
+    [v]
     (math/sqrt (dot-prod v v)))
 
-(defn norm [v]
+(defn norm 
     "Returns unit vector of v."
+    [v]
     (map #(/ % (vec-len v)) v))
 
-(defn distance [u v]
+(defn distance 
     "Returns the distance between u and v."
+    [u v]
     (vec-len (vec-add u (neg v))))
 
-(defn vec-angle [u v]
+(defn vec-angle 
     "Returns the angle in degrees between vector u and v."
+    [u v]
     (* (/ 180 Math/PI) 
        (Math/acos (/ (dot-prod u v) 
                   (* (vec-len u) (vec-len v))))))
