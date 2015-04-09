d8213c3b51eb8dbbdd6dbedd35be2343b6cccd74
SUNEEJ <suneejnair@gmail.com>
2015-04-10 00:06:50 +0200

    Added a function to calculate the angle between vectors.

--- src/poole_algebra/core.clj
+++ src/poole_algebra/core.clj
@@ -31,8 +31,9 @@
 
 (defn vec-angle [u v]
 "Returns the angle in degrees between vector u and v."
-    (* (/ 180 Math/PI) (Math/acos (/ (dot-prod u v) 
-                       (* (vec-len u) (vec-len v))))))
+    (* (/ 180 Math/PI) 
+       (Math/acos (/ (dot-prod u v) 
+                  (* (vec-len u) (vec-len v))))))
 
 
 
